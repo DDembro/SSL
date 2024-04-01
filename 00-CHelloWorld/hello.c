fb298570a9f55ef111f50a805da8db9e32bdc1b6
DDembro <dembrowkynicolasa@gmail.com>
2024-04-01 17:47:06 -0300

    Trabajo 0

---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,12 @@
+/* hello.c
+ * Programa que imprime Hello World
+ * Dembrowky Nicolas
+ * 20240401
+*/
+
+#include <stdio.h>
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+}
--- 00-CHelloWorld/readme.md
+++ 00-CHelloWorld/readme.md
@@ -1,1 +1,3 @@
-Read Me
+### 00-CHelloWorld
+
+* Compilador: GCC versión 10.3.0 (tdm64-1) | Compila hasta std c2x
